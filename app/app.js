dff0749ce006acc8ad23f0bdb24b91e5be35c9db
gras1 <graham.fearn@coralinsurance.com>
2015-05-19 10:33:36 +0100

    Initial Check In

---
+++ app/app.js
@@ -0,0 +1,39 @@
+﻿/// <reference path="../scripts/angular.min.js" />
+
+/*##########################################################################
+
+  Normally like to break AngularJS apps into the following folder structure
+  at a minimum
+
+  /app
+    /controllers
+    /directives
+    /services
+    /partials
+    /views
+############################################################################*/
+
+var app = angular.module('customersApp', []);
+
+//This configures the routes and associates each route with a view an
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/customers',
+        {
+            controller: 'CustomersController',
+            templateUrl: '/app/partials/customers.html'
+        })
+        //Define a route that has a route paramter in it (:customerID
+        .when('/customerorders/:customerID',
+        {
+            controller: 'CustomerOrdersController',
+            templateUrl: '/app/partials/customerOrders.html'
+        })
+        //Define a route that has a route parameter in it (:customerID
+        .when('/orders',
+        {
+            controller: 'OrdersController',
+            templateUrl: '/app/partials/orders.html'
+        })
+        .otherwise({ redirectTo: '/customers' });
+});
---
+++ app/controllers/controllers.js
@@ -0,0 +1,53 @@
+﻿/*##########################################################################
+
+  Normally like to break AngularJS controllers into separate files.
+  Kept them together here since they're small and it's easier to look
+  example
+
+##########################################################################*/
+
+//This controller retrieves data from the customersService and associ
+//The $scope is ultimately bound to the customers view
+app.controller('CustomersController', function($scope, customersService) {
+    
+    //I like to have an init() for controllers that need to perform s
+    //one place...not required though especially in the simple exampl
+    init();
+
+    function init() {
+        $scope.customers = customersService.getCustomers();
+    }
+
+    $scope.insertCustomer = function() {
+        var firstName = $scope.newCustomer.firstName;
+        var lastName = $scope.newCustomer.lastName;
+        var city = $scope.newCustomer.city;
+        customersService.insertAdjacentElement(firstName, lastName, city);
+        $scope.newCustomer.firstName = '';
+        $scope.newCustomer.lastName = '';
+        $scope.newCustomer.city = '';
+    };
+
+    $scope.deleteCustomer = function(id) {
+        customersService.deleteCustomer(id);
+    };
+});
+
+//This controller retrieves data from the customersService and associ
+//The $scope is bound to the order view
+app.controller('CustomerOrdersController', function($scope, $routeParams) {
+    $scope.customers = {};
+    $scope.ordersTotal = 0.00;
+
+    //I like to have an init() for controllers that need to perform s
+    //one place...not required though especially in the simple exampl
+    init();
+
+    function init() {
+        //Grab customerID off of the route
+        var customerID = ($routeParams.customerID) ? parseInt($routeP
+        if (customerID > 0) {
+            $scope.customer = customersService.getCustomer(customerID);
+        }
+    }
+});
